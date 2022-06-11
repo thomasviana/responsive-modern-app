b3cbe5647b8c6431db3025bae51ea76c49782a02
Thomas Viana <thomasviana16@gmail.com>
2022-06-11 14:03:50 -0500

    Finish brands component.

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,24 @@
 import "./brand.css";
+import { atlassian, dropbox, google, shopify, slack } from "./imports";
 
-const Brand = () => {
-    return (
-        <div>Brand</div>
-    )
-}
+const Brand = () => (
+ <div className='gpt3__brand section__padding'>
+  <div>
+   <img src={google} />
+  </div>
+  <div>
+   <img src={slack} />
+  </div>
+  <div>
+   <img src={atlassian} />
+  </div>
+  <div>
+   <img src={dropbox} />
+  </div>
+  <div>
+   <img src={shopify} />
+  </div>
+ </div>
+);
 
-export default Brand
+export default Brand;
